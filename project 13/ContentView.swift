97dc2ae4d3c9aac30d93a62c879752e7e38cc2ce
Anisha Lamichhane <ansalamichhane111@gmail.com>
2021-06-14 13:52:14 +0545

    custom binding

--- project 13/ContentView.swift
+++ project 13/ContentView.swift
@@ -8,19 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0 {
-        didSet {
-            print("The new value is \(blurAmount)")
-        }
-    }
+    @State private var blurAmount: CGFloat = 0
        
     
     var body: some View {
-        VStack {
+        let blur = Binding<CGFloat>(
+            get:  {
+                self.blurAmount
+            },
+        set: {
+            self.blurAmount = $0
+            print("New value is \(self.blurAmount)")
+        }
+        )
+        
+       return VStack {
         Text("Hello, world!")
             .blur(radius: blurAmount)
             
-            Slider(value: $blurAmount, in: 1...100)
+            Slider(value: blur, in: 1...100)
         }
     }
 }
